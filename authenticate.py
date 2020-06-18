5bdd9c261f1b86e9aa573d02bde7a04933eb8176
tylerkotler <54518126+tylerkotler@users.noreply.github.com>
2020-06-18 16:17:21 -0400

    Uploading authenticate file

---
+++ authenticate.py
@@ -0,0 +1,8 @@
+#Fill in your information below in between the quotation marks for each variable
+#(Look at READ.me for info on getting each one of these)
+
+username=''
+client_id=''
+client_secret=''
+redirect_uri='http://localhost:8080'
+spotify_id = ''
